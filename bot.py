12dbd626c83c335b228bf27502dde662ed91039f
ZainRaza116 <syedzaynraza@gmail.com>
2024-01-25 21:51:13 +0500

    Your commit message

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from openpyxl import Workbook, load_workbook
-# import os
-
 
 app = Flask(__name__, static_folder='static')
-
 
 @app.route('/deposit/<deposit_id>')
 def deposit(deposit_id):
     return render_template('index2.html', deposit_id=deposit_id)
 
-
 if __name__ == '__main__':
-    app.run(port=8000 , debug=True)
+    app.run(port=8000, debug=True)
